92fd4074b2d94eb94ff4f8e62e73fcc7532bfea6
Oiza <oizadigitech@gmail.com>
2024-05-15 02:20:04 +0100

    new changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,112 @@
 import React from "react";
 
-function Header() {
+import logo from "../img/logo.png";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  List,
+  ListItem,
+  styled,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+// menu
+import DrawerItems from "../DrawerItems/DrawerItems";
+// rotas
+import { Outlet, Link } from "react-router-dom";
+
+// personalizacao
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const ListMenu = styled(List)(({ theme }) => ({
+  display: "none",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+//rotas
+const itemList = [
+  {
+    text: "Home",
+    to: "/",
+  },
+  {
+    text: "Course",
+    to: "/course",
+  },
+  {
+    text: "Instructor",
+    to: "/instructor",
+  },
+  {
+    text: "Internship",
+    to: "/internship",
+  },
+  {
+    text: "Certificate",
+    to: "/certificate",
+  },
+];
+
+function Layout() {
   return (
     <div>
-      <header>
-        <h1>
-          <img src=""></img>Zidio <br />
-          Learning
-        </h1>
-        <button>
-          <p>Quick search</p>
-        </button>
-        <button>
-          <p>User 1</p>
-        </button>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <hr></hr>
-      </header>
+      <AppBar
+        component="nav"
+        position="sticky"
+        sx={{
+          backgroundColor: "rgba(66, 148, 243, 1)",
+        }}
+        elevation={0}
+      >
+        <StyledToolbar>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={{
+              width: "20%",
+              height: "10vh",
+              marginTop: "10px",
+              margintop: "-70px",
+            }}
+          />
+
+          <Box sx={{ display: { xs: "block", sm: "none" } }}>
+            <DrawerItems />
+          </Box>
+          <ListMenu>
+            {itemList.map((item) => {
+              const { text } = item;
+              return (
+                <ListItem key={text}>
+                  <ListItemButton
+                    component={Link}
+                    to={item.to}
+                    sx={{
+                      color: "#fff",
+                      "&:hover": {
+                        backgroundColor: "transparent",
+                        color: "black",
+                      },
+                    }}
+                  >
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </ListMenu>
+        </StyledToolbar>
+      </AppBar>
+      <Outlet />
     </div>
   );
 }
 
-export default Header;
+export default Layout;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,110 @@
 import React from "react";
-import Home from "../Home/Home";
+
+import logo from "../img/logo.png";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  List,
+  ListItem,
+  styled,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+// menu
+import DrawerItems from "../DrawerItems/DrawerItems";
+// rotas
+import { Outlet, Link } from "react-router-dom";
+
+// personalizacao
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const ListMenu = styled(List)(({ theme }) => ({
+  display: "none",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+//rotas
+const itemList = [
+  {
+    text: "Home",
+    to: "/",
+  },
+  {
+    text: "Course",
+    to: "/course",
+  },
+  {
+    text: "Instructor",
+    to: "/instructor",
+  },
+  {
+    text: "Internship",
+    to: "/internship",
+  },
+  {
+    text: "Certificate",
+    to: "/certificate",
+  },
+];
 
 function Layout() {
   return (
     <div>
-      <Home />
+      <AppBar
+        component="nav"
+        position="sticky"
+        sx={{
+          backgroundColor: "rgba(66, 148, 243, 1)",
+        }}
+        elevation={0}
+      >
+        <StyledToolbar>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={{
+              width: "20%",
+              height: "10vh",
+              marginTop: "10px",
+              margintop: "-70px",
+            }}
+          />
+
+          <Box sx={{ display: { xs: "block", sm: "none" } }}>
+            <DrawerItems />
+          </Box>
+          <ListMenu>
+            {itemList.map((item) => {
+              const { text } = item;
+              return (
+                <ListItem key={text}>
+                  <ListItemButton
+                    component={Link}
+                    to={item.to}
+                    sx={{
+                      color: "#fff",
+                      "&:hover": {
+                        backgroundColor: "transparent",
+                        color: "black",
+                      },
+                    }}
+                  >
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </ListMenu>
+        </StyledToolbar>
+      </AppBar>
+      <Outlet />
     </div>
   );
 }
